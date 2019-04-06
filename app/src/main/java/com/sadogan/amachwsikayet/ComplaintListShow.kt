7ce2dae8dfb53a915dafc25eae22ec721ce9fd0f
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2019-04-06 19:49:10 +0300

    complaint show

---
+++ app/src/main/java/com/sadogan/amachwsikayet/ComplaintListShow.kt
@@ -0,0 +1,12 @@
+package com.sadogan.amachwsikayet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ComplaintListShow : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complaint_list_show)
+    }
+}
